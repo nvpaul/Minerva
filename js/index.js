5d71d5c747796478b297782890bb4a00368624c4
Naomi <63750118+nvpaul@users.noreply.github.com>
2020-11-18 12:20:46 -0500

    Initial upload of files

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$("#investments-btn").click(function() {
+    $("#investments-icon").toggleClass('fa-caret-right fa-sort-down');
+  });
+
+  $("#benefits-btn").click(function() {
+    $("#benefits-icon").toggleClass('fa-caret-right fa-sort-down');
+  });
